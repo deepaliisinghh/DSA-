97f4a592145e3705ecffabba44e659561b712184
deepaliisinghh <deepalisingh4659@gmail.com>
2026-02-24 21:02:22 +0530

    Add files via upload

---
+++ horseshoe.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int s1,s2,s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    
+    int count=0;
+    if((s1==s2) || (s1==s3) || (s1==s4))
+    {
+        count++;
+    }
+    else if((s2==s3)|| (s2==s4))
+    {
+        count++;
+    }
+    else if((s3==s4))
+    {
+        count++;
+    }
+    cout<<count;
+}
