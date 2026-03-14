7c3d86b39d38ae359860ed7d82e8da314a797c0f
deepaliisinghh <deepalisingh4659@gmail.com>
2026-03-14 20:44:47 +0530

    Add files via upload

---
+++ Helpful Maths.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    vector<char> v;
+
+    for(int i=0;i<s.size();i++){
+        if(s[i] != '+'){
+            v.push_back(s[i]);
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0;i<v.size();i++){
+        cout << v[i];
+        if(i != v.size()-1)
+            cout << "+";
+    }
+
+    return 0;
+}
